5fe1bc7e62c24eedbfe06e17e85160b8b08a00a5
Babeena <babeenak020@gmail.com>
2023-09-07 11:38:45 +0530

    Add project files.

---
+++ CodefirstStudent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System;
+
+namespace CodefirstStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
